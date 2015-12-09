72157cf85246e8c5f91cd465d4dc5784b3e08aef
muurastr <muura@mail.ru>
2015-12-09 20:24:02 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -8,13 +8,18 @@ def init_db
   @db = SQLite3::Database.new 'lepra.db'
   @db.results_as_hash = true
 end
-
+# before вызывается каждый раз при перезагрузке любой страницы
 before do
-    init_db
+    init_db 
+    # инициализация БД
 end
+
+# configure вызывается каждый раз при конфигурации приложения:
+# когда изменился код программы И перезаерузилась страница
 
 configure do
   init_db
+  # инициализация БД
   @db.execute 'Create table if not exists Posts
   (
       id integer primary key autoincrement,
@@ -26,12 +31,16 @@ end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
-
+# обзаботчик get-запроса /new
+# (браузер получает страницу с сервера)
 get '/new' do
   erb :new 
 end
 
+# обработчик post-запроса /new
+# (браузер отправляет данные на сервер)
 post '/new' do
+  # получаем переменную из post-запроса 
   @content = params[:content]
 
   erb "You typed #{content}"
